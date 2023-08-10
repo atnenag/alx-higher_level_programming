15e0801fbe78433e508bc3e9f06c21b3b5afabfb
atnenag <atnenag@gmail.com>
2023-08-10 00:31:12 -0700

    all the remaining tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for alphabet in range(122, 96, -1):
-    ASCII_alphabet = chr(alphabet)
-    if alphabet % 2 == 0:
-        ASCII_alphabet = ASCII_alphabet.lower()
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        letter = i
     else:
-        ASCII_alphabet = ASCII_alphabet.upper()
-    print(f"{ASCII_alphabet}", end="")
+        letter = i - 32
+    print("{:s}".format(chr(letter)), end=""))
