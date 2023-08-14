05678b1a8a788d371934aace9f7dd9e8120a5428
atnenag <atnenag@gmail.com>
2023-08-14 12:22:50 -0700

    Missed ones 3; 6; and 9

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for value in matrix:
-        for integer in value:
-            print("{:d}".format(integer), end=" ")
+    for row in range(len(matrix)):
+        count = 0
+        for val in matrix[row]:
+            print("{:d}".format(val), end="")
+            if count < len(matrix[row]) - 1:
+                print(end=" ")
+            count += 1
         print()
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if len(my_list)== 0:
         return None
-    for i in range(len(my_list)):
-        if my_list[i] > my_list[i + 1]:
-            maxim = my_list[i]
-            return maxim
+    else:
+        maxi = my_list[0]
+        for i in my_list:
+            if i > maxi:
+                maxi = i
+        return (maxi)m
