51b127e952accd256d3f5beee04042bf099a121e
Atnafu <atnenag@gmail.com>
2024-04-21 23:49:52 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
+    change_case = 0
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            change_case = 32
+        else:
+            change_case = 0
+        print('{:c}'.format(ord(letter) - change_case), end="")
     print()
