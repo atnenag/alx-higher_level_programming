d083987fc5914757f766a55035fbefee491fe8c0
Atnafu <atnenag@gmail.com>
2024-04-23 23:24:43 -0700

    Module 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
+
+
+def main():
+    print('{} argument'.format(len(argv) - 1), end='')
+    if len(argv) == 1:
+        print('s.')
+    elif len(argv) == 2:
+        print(':')
+    else:
+        print('s:')
+    for i in range(1, len(argv)):
+        print('{}: {}'.format(i, argv[i]))
+
 if __name__ == "__main__":
-    num_arguments = len(argv)
-    if num_arguments == 0:
-        print("0 arguments.")
-    elif num_arguments == 1:
-        print("1 argument:")
-    else:
-        print(f"{num_arguments} arguments:")
-    if num_arguments == 0:
-        print(".")
-    else:
-        for i, arg in enumerate(argv, start=1):
-            print(f"{i}: {arg}")
+    main()
